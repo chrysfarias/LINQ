f046e5794542223a3f150bf83428c1c472e54a5e
chrystian farias <chrystianfarias@hotmail.com>
2022-04-06 10:44:17 -0400

    first linq query with filter

--- ExemplosLinq/Program.cs
+++ ExemplosLinq/Program.cs
@@ -1,47 +1,58 @@
 ﻿
 
-//fonte de dados // list
+        //Data Source // list
 
-var listaProdutos = new List<Produto>()
-{ 
-    new Produto { Id = 1, CategoriaId = 3, Nome = "Ventilador", Status = true, Valor = 120 },
-    new Produto { Id = 2, CategoriaId = 2, Nome = "Televisor", Status = true, Valor = 400 },
-    new Produto { Id = 3, CategoriaId = 3, Nome = "Camiseta", Status = true, Valor = 90 },
-    new Produto { Id = 4, CategoriaId = 3, Nome = "Sapato", Status = true, Valor = 200 },
-    new Produto { Id = 5, CategoriaId = 1, Nome = "Mesa", Status = true, Valor = 120 },
-    new Produto { Id = 6, CategoriaId = 3, Nome = "Fogão", Status = true, Valor = 300 },
-    new Produto { Id = 7, CategoriaId = 3, Nome = "Computador", Status = true, Valor = 1200 },
-    new Produto { Id = 8, CategoriaId = 2, Nome = "Bola", Status = true, Valor = 40 },
-    new Produto { Id = 9, CategoriaId = 1, Nome = "Celular", Status = true, Valor = 420 }
-};
+        var productList = new List<Product>()
+        { 
+            new Product { Id = 1, CategoryId = 3, Name = "Fan", Status = true, Value = 120 },
+            new Product { Id = 2, CategoryId = 2, Name = "Television", Status = true, Value = 400 },
+            new Product { Id = 3, CategoryId = 3, Name = "T-shirt", Status = true, Value = 90 },
+            new Product { Id = 4, CategoryId = 3, Name = "Shose", Status = true, Value = 200 },
+            new Product { Id = 5, CategoryId = 1, Name = "Table", Status = true, Value = 120 },
+            new Product { Id = 6, CategoryId = 3, Name = "Stove", Status = true, Value = 300 },
+            new Product { Id = 7, CategoryId = 3, Name = "Computer", Status = true, Value = 1200 },
+            new Product { Id = 8, CategoryId = 2, Name = "Ball", Status = true, Value = 40 },
+            new Product { Id = 9, CategoryId = 1, Name = "Cellphone", Status = true, Value = 420 }
+        };
 
 
-var listaCategorias = new List<Categoria>()
-{
-    new Categoria{ Id = 1, Nome = "Eletrônicos", Status = true },
-    new Categoria{ Id = 2, Nome = "Outros", Status = true },
+        var categoryLists = new List<Category>()
+        {
+            new Category{ Id = 1, Name = "Eletronics", Status = true },
+            new Category{ Id = 2, Name = "Others", Status = true },
    
-};
+        };
 
-// consulta linq    
+        // linq query
+
+        var result = from product in productList
+             where product.Value > 400
+             select product;
 
 
-class Produto
-{
-    public int Id { get; set; }
-    public string Nome { get; set; }
-    public bool Status { get; set; }
-    public decimal  Valor { get; set; }
-    public int CategoriaId { get; set; }
+        foreach (var product in result)
+        {
+            Console.WriteLine($"{product.Id}|{product.Name}|{product.Status}|{product.Value}|");
+        
+        }
 
-}
 
-class Categoria
-{
-    public int Id { get; set; }
+        class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public bool Status { get; set; }
+            public decimal Value { get; set; }
+            public int CategoryId { get; set; }
 
-    public string Nome { get; set; }
+        }
 
-    public bool Status { get; set; }
+        class Category
+        {
+            public int Id { get; set; }
 
-}
+            public string Name { get; set; }
+
+            public bool Status { get; set; }
+
+        }
